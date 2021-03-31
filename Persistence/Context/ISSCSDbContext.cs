747ca1566eaac8106a51c64a8d2bd5a598aa0f26
MkTech-0-8YT3 <mktech@protonmail.com>
2021-03-31 17:55:22 +0200

    Add persistence layer

---
+++ Persistence/Context/ISSCSDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Common.DataModels.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public interface ISSCSDbContext
+    {
+        DatabaseFacade Database { get; }
+        public DbSet<Student> Accounts { get; set; }
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Persistence/Context/SSCSDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Common.DataModels.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    class SSCSDbContext : DbContext, ISSCSDbContext
+    {
+        public SSCSDbContext(DbContextOptions<SSCSDbContext> options) : base(options) { }
+
+        public DbSet<Student> Accounts { get; set; }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
