d188c96559fca3025f0570e233a41daba4bd9b24
MkTech-0-8YT3 <mktech@protonmail.com>
2021-03-31 20:35:28 +0200

    Introduce client with basic card reader implementation

---
+++ Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using PCSC;
+using PCSC.Monitoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var readerName = "Gemplus USB SmartCard Reader 0";
+            var contextFactory = ContextFactory.Instance;
+            var monitorFactory = MonitorFactory.Instance;
+            using var context = contextFactory.Establish(SCardScope.System);
+            var monitor = monitorFactory.Create(SCardScope.System);
+
+            monitor.CardInserted += (sender, args) =>
+                Console.WriteLine($"Cart ATR: {GetReadableCardAtr(args.Atr)}");
+            
+            monitor.Start(readerName);
+            
+            Console.ReadKey(); // key to exit
+
+            monitor.Cancel();
+            monitor.Dispose();
+        }
+
+        private static string GetReadableCardAtr(byte[] atr)
+        {
+            if (atr == null || atr.Length == 0)
+            {
+                return "";
+            }
+
+            return BitConverter.ToString(atr);
+        }
+    }
+}
